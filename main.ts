9580a94f22be8bc32553506565655042d2ce1103
Phatisena <naphatism@hotmail.com>
2024-12-06 11:10:29 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,43 @@
+namespace math {
+
+    export function CheckNumStr(numstri:string="") {
+        for (let _i = 0; _i < numstri.length; _i++) {
+            if (!("0123456789".includes(numstri.charAt(_i)))) {
+                return false
+            }
+        }
+        return true
+    }
+
+    export function AurismCheck(numstring:string="",remnum:number=0,invert:boolean=false,reverse:boolean=false) {
+        if (!(CheckNumStr(numstring))) { return -1 }
+        let numlist: number[] = []
+        for (let _i = 0; _i < numstring.length; _i++) {
+            numlist.push(parseInt(numstring.charAt(_i)))
+        }
+        let numsum: number[] = []
+        let numv = 0
+        let curv = 0
+        for (let _i = 0; _i < numlist.length; _i++) {
+            if (invert) {
+                numv -= 1
+                if (numv <= 0) {curv += 1;numv = curv}
+            } else {
+                if (curv <= 0) {curv = 1}
+                numv += 1
+                if (numv > curv) {curv += 1;numv = 1}
+            }
+            if (reverse) {numsum.unshift(numv)} else {numsum.push(numv)}
+        }
+        let numcheck: number[] = []
+        numv = 0
+        curv = 0
+        for (let _i = 0; _i < numlist.length; _i++) {
+            numv = numlist[_i] * numsum[_i]
+            numcheck.push(numv)
+            curv += numv
+        }
+        if (remnum > 0) { curv = curv % remnum}
+        return curv
+    }
+}
